60e38efc6ffd1bfa238249379c6a1754dd9fba3f
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-08-25 22:09:05 -0300

    Ajuste na responsividade na tela de Login

--- src/pages/Signin/Index.jsx
+++ src/pages/Signin/Index.jsx
@@ -5,11 +5,11 @@ import styles from './styles.module.css'
 export default function Signin() {
     return (
         <div>
-            <Container className={styles.alinhamento}>
-                <Row className="d-flex justify-content-center align-content-center bg-dark h-75 w-75">
-                    <Col sm={12} md={8} lg={6} xl={5} className="w-50">
-                        <Card className="card shadow-2-strong min-vh-10 w-100"  >
-                            <Card.Body className="card-body p-5 text-center">
+            <Container className={styles.conteiner}>
+                <Row className={`${styles.linha} ${'bg-dark'}`}>
+                    <Col sm={12} md={8} lg={6} xl={5} className={styles.coluna}>
+                        <Card className={`${styles.cartao}`}  >
+                            <Card.Body className=" p-5 text-center">
 
                                 <h3 className="mb-5">Login</h3>
                                 <FloatingLabel controlId="floatingInput" label="E-mail" className="mb-3">
