45661c9e535d2f45014ea52168cea66512a79a25
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-08-27 21:03:31 -0300

    Alterações de Interface

--- src/components/cabecalho/Index.jsx
+++ src/components/cabecalho/Index.jsx
@@ -1,4 +1,5 @@
 import { Container, Navbar, Row, Col } from "react-bootstrap";
+import { Link } from "react-router-dom";
 import logo from "../../assets/IFBVertical.png";
 import styles from "./styles.module.css";
 
@@ -7,10 +8,12 @@ export default function Header() {
     <Navbar className="bg-dark text-white">
       <Container fluid>
         <Row>
-          <Col md={3} className={styles.cx_img}>
-            <img className={styles.img} src={logo} alt="Img Logo" />
+          <Col md={4} >
+            <Navbar.Brand as={Link} to="/" className="d-flex flex-column justify-content-center align-items-center">
+              <img className={styles.img} src={logo} alt="Icone de eventos" />
+            </Navbar.Brand>
           </Col>
-          <Col md={9} className="d-flex flex-column justify-content-center align-items-center">
+          <Col md={8} className="d-flex flex-column justify-content-center align-items-center">
             <h1> Sistema de Gerenciamento de Presença em Eventos </h1>
           </Col>
         </Row>
--- src/pages/Principal/Index.jsx
+++ src/pages/Principal/Index.jsx
@@ -1,49 +1,77 @@
 import { Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
+import {Nav}  from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import styles from "./styles.module.css";
 
 import logo from "../../assets/icone_evento.png"
+import icone_evento from "../../assets/calendario.png"
+import icone_usuario from "../../assets/pesquisar_usuario.png"
 
 export default function Principal() {
     return (
         <Container fluid >
-            <Row>
-                <Col sm={12} md={6} lg={6} className={styles.caixa_comprimento}>
+            {/* Linha superior  */}
+            <Row >
+                <Col sm={12} md={6} lg={6} className={styles.caixa_comprimento} >
                     <Container className={`${styles.caixa}`}>
-                        <div className={`${styles.cx_interna} ${'text-white'}`}>
-                         <img className={styles.card_img} src={logo} alt="Img Logo" /> 
-                         {/* <Button variant="outline-dark" className={styles.button} > <Link to='/eventos' className={`${styles.link}`}> Eventos </Link></Button> */}
-                         <Link to='/eventos'> <Button variant="outline-dark" size="lg"> Eventos </Button>  </Link>
-                        </div>
+                        <Container className={`${styles.cx_interna}`}>
+                            {/* Icone com Link */}
+                            <Nav.Link as={Link} to="/eventos" className="d-flex flex-column justify-content-center align-items-center" > 
+                                <img className={styles.card_img} src={icone_evento} alt="Img Logo"/>  
+                            </Nav.Link>  
+                            {/* Botão para Evento  */}
+                            <Link to='/eventos'> 
+                                <Button variant="outline-dark" size="lg"> Eventos </Button>  
+                            </Link>
+                        </Container>
                     </Container>
                 </Col>
                 <Col sm={12} md={6} lg={6} className={styles.caixa_comprimento}>
                     <Container className={`${styles.caixa}`}>
-                        <div className={`${styles.cx_interna} ${'text-white'}`}>
-                        <img className={styles.card_img} src={logo} alt="Img Logo" />
-                         <Button variant="outline-dark" size="lg" >Pesquisar Usuários</Button> 
-                        </div>
+                        <Container className={`${styles.cx_interna}`}>
+                            {/* Icone com Link */}
+                            <Nav.Link as={Link} to="/#" className="d-flex flex-column justify-content-center align-items-center" > 
+                                <img className={styles.card_img} src={icone_usuario} alt="Img Logo"/>  
+                            </Nav.Link>  
+                            {/* Botão para Pesquisar Usuario  */}
+                            <Link to='/eventos'> 
+                                <Button variant="outline-dark" size="lg"> Pesquisar Usuários </Button>  
+                            </Link>
+                        </Container>
                     </Container>
                 </Col>
             </Row>
+            {/* Linha Infeiror  */}
             <Row>
-            <Col sm={12} md={6} lg={6} className={styles.caixa_comprimento}>
-                    <Container className={`${styles.caixa}`}>
-                        <div className={`${styles.cx_interna} ${'text-white'}`}>
-                        <img className={styles.card_img} src={logo} alt="Img Logo" />
-                         <Button variant="outline-dark" className={styles.button} >Eventos</Button> 
-                        </div>
-                    </Container>
-                </Col>
                 <Col sm={12} md={6} lg={6} className={styles.caixa_comprimento}>
-                    <Container className={`${styles.caixa}`}>
-                        <div className={`${styles.cx_interna} ${'text-white'}`}>
-                        <img className={styles.card_img} src={logo} alt="Img Logo" />
-                         <Button variant="outline-dark" className={styles.button} >Eventos</Button> 
-                        </div>
-                    </Container>
-                </Col>
+                        <Container className={`${styles.caixa}`}>
+                        <Container className={`${styles.cx_interna}`}>
+                            {/* Icone com Link */}
+                            <Nav.Link as={Link} to="/#" className="d-flex flex-column justify-content-center align-items-center" > 
+                                <img className={styles.card_img} src={logo} alt="Img Logo"/>  
+                            </Nav.Link>  
+                            {/* Botão para Evento  */}
+                            <Link to='/eventos'> 
+                                <Button variant="outline-dark" size="lg"> Pesquisar Usuários </Button>  
+                            </Link>
+                        </Container>
+                        </Container>
+                    </Col>
+                    <Col sm={12} md={6} lg={6} className={styles.caixa_comprimento}>
+                        <Container className={`${styles.caixa}`}>
+                        <Container className={`${styles.cx_interna}`}>
+                            {/* Icone com Link */}
+                            <Nav.Link as={Link} to="/#" className="d-flex flex-column justify-content-center align-items-center" > 
+                                <img className={styles.card_img} src={logo} alt="Img Logo"/>  
+                            </Nav.Link>  
+                            {/* Botão para Evento  */}
+                            <Link to='/eventos'> 
+                                <Button variant="outline-dark" size="lg"> Pesquisar Usuários </Button>  
+                            </Link>
+                        </Container>
+                        </Container>
+                    </Col>
                 {/* <Col sm={12} md={6} lg={6} className="border">
                     <p> Caixa Principal 4 </p>
                     <h1> Pagina Principal </h1>
