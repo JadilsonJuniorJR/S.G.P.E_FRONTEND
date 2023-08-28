ca6fc57f688ddc1c0e00f614047a5d23251e9d19
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-08-27 21:38:00 -0300

    Atualizações de Formulário
    Adicionando o States

--- src/pages/Eventos/index.jsx
+++ src/pages/Eventos/index.jsx
@@ -1,16 +1,24 @@
 import { Container, Row, Col } from "react-bootstrap";
 // import { Link } from "react-router-dom";
 
-
 import Button from 'react-bootstrap/Button';
 import Form from 'react-bootstrap/Form';
 
 // import logo from "../../assets/icone_evento.png"
 import styles from "./styles.module.css";
-
-
+import { useState } from "react";
 
 export default function Eventos() {
+
+  const [nome_evento, setNome_Evento] = useState('');
+  const [organizador, setOrganizador] = useState('');
+  const [descricao, setDescricao] = useState('');
+
+  const handleSubmit = (evento) =>{
+    evento.preventDefault();
+    alert("clicou")
+  }
+
   return (
     <Container>
       <Row>
@@ -21,20 +29,26 @@ export default function Eventos() {
 
               <Form.Group className="mb-3" controlId="formBasicNomeEvento">
                 <Form.Label>Nome do Evento: </Form.Label>
-                <Form.Control type="text" placeholder="Digite o nome do evento:" />
+                <Form.Control type="text" placeholder="Digite o nome do evento:"
+                  value={nome_evento}
+                  onRateChange={(e) => setNome_Evento(e.target.value)} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="formBasicOrganizador">
                 <Form.Label>Organizador:</Form.Label>
-                <Form.Control type="text" placeholder="Digite o nome do organizador:" />
+                <Form.Control type="text" placeholder="Digite o nome do organizador:"
+                  value={organizador}
+                  onRateChange={(e) => setOrganizador(e.target.value)} />
               </Form.Group>
 
               <Form.Group className="mb-3" controlId="exampleForm.ControlTextarea1">
                 <Form.Label>Descrição:</Form.Label>
-                <Form.Control as="textarea" className={styles.form_tamanho} rows={3} />
+                <Form.Control as="textarea" className={styles.form_tamanho} rows={3}
+                  value={descricao}
+                  onRateChange={(e) => setDescricao(e.target.value)} />
               </Form.Group>
 
-              <Button variant="primary" type="submit">
+              <Button variant="primary" type="submit" onClick={handleSubmit}>
                 Enviar
               </Button>
             </Form>
