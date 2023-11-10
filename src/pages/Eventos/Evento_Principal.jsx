1e590eb5a265d405361ed8a65bccbc44b7790600
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-11-09 21:31:54 -0300

    Importação de icones

--- src/pages/Eventos/Evento_Principal.jsx
+++ src/pages/Eventos/Evento_Principal.jsx
@@ -4,22 +4,21 @@ import { Nav } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import styles from './styles.module.css';
 
-import logo from "../../assets/icone_evento.png"
-import icone_evento from "../../assets/evento_criar.png"
-import icone_usuario from "../../assets/pesquisar_evento.png"
 
+import icone_evento from "../../assets/evento_criar.png"
+import icone_registro from "../../assets/pesquisar_evento.png"
+import icone_listar from "../../assets/listar_eventos.png"
 
 
 export default function Eventos() {
 
     return (
-        <Container>
+        <Container className={`${styles.caixa_principal} `}>
             {/* Linha superior  */}
 
             <Row >
                 <Col sm={12} md={6} lg={4} className={`${styles.caixa_secundaria} `} >
                     <Container className={`${styles.caixa_terciaria} `}>
-
                         {/* Icone com Link */}
                         <Link as={Link} to="/inicio/eventos" className={`${styles.links}`} >
                             <img className={styles.card_img} src={icone_evento} alt="Icones de Criar Eventos" />
@@ -28,7 +27,6 @@ export default function Eventos() {
                         <Link to='/inicio/eventos'>
                             <Button variant="outline-dark" className={`${styles.botao} `} size="lg"> Criar Eventos </Button>
                         </Link>
-
                     </Container>
                 </Col>
                 <Col sm={12} md={6} lg={4} className={styles.caixa_secundaria}>
@@ -36,7 +34,7 @@ export default function Eventos() {
 
                         {/* Icone com Link */}
                         <Link to="/inicio/Evento_Registro" className={`${styles.links}`} >
-                            <img className={styles.card_img} src={icone_usuario} alt="Img Logo" />
+                            <img className={styles.card_img} src={icone_registro} alt="Img Logo" />
                         </Link>
                         {/* Botão para Pesquisar Usuario  */}
                         <Link to='/inicio/Evento_Registro'>
@@ -50,12 +48,12 @@ export default function Eventos() {
                     <Container className={`${styles.caixa_terciaria}`}>
 
                         {/* Icone com Link */}
-                        <Nav.Link as={Link} to="/inicio/evento_arquivar" className="d-flex flex-column justify-content-center align-items-center" >
-                            <img className={styles.card_img} src={logo} alt="Img Logo" />
+                        <Nav.Link as={Link} to="/inicio/evento_lista" className="d-flex flex-column justify-content-center align-items-center" >
+                            <img className={styles.card_img} src={icone_listar} alt="Img Logo" />
                         </Nav.Link>
                         {/* Botão para Evento  */}
-                        <Link to='/inicio/evento_arquivar'>
-                            <Button variant="outline-dark" className={`${styles.botao} `} size="lg"> Arquivar Evento </Button>
+                        <Link to='/inicio/evento_lista'>
+                            <Button variant="outline-dark" className={`${styles.botao} `} size="lg"> Listas do Evento</Button>
                         </Link>
 
                     </Container>
