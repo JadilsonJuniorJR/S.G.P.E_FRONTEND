7b0d205f75fc4ee243e5a0dfbde8580b2b59fdfe
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-12-04 17:54:41 -0300

    Update axiosInstance.js

--- src/axios/axiosInstance.js
+++ src/axios/axiosInstance.js
@@ -3,7 +3,8 @@ import axios from "axios"
 //const sgpeFetch = axios
 
 const axiosInstance = axios.create({
-    baseURL: "http://localhost:3001",
+    // baseURL: "http://localhost:3001",
+    baseURL: "https://s-g-p-e-backend.vercel.app/"
 });
 
 export default axiosInstance;
