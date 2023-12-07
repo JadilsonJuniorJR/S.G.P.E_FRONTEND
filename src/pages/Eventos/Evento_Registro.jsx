7da0ff36ed1b0d30b7a9864875bbcfce43475cde
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-12-06 21:12:09 -0300

    Update Evento_Registro.jsx

--- src/pages/Eventos/Evento_Registro.jsx
+++ src/pages/Eventos/Evento_Registro.jsx
@@ -58,7 +58,7 @@ export default function Evento_Registro() {
                                         <td> {registro.nome_evento}</td>
                                         <td> {registro.fk_id_adm}</td>
                                         <td> {moment.utc(registro.data_inicio).format('DD-MM-YYYY')}</td>
-                                        <td> {moment(registro.data_termino).format('DD/MM/YYYY')}</td>
+                                        <td> {moment(registro.data_termino).format('DD-MM-YYYY')}</td>
                                         <td> {registro.descricao}</td>
                                     </tr>
                                 )
