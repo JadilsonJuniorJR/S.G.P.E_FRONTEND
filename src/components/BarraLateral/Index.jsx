c5ec20f875795b0369087a7d16251028b6d933f6
Jadilson Junior <74326526+JadilsonJR@users.noreply.github.com>
2023-12-07 11:55:20 -0300

    Update Index.jsx

--- src/components/BarraLateral/Index.jsx
+++ src/components/BarraLateral/Index.jsx
@@ -1,6 +1,6 @@
 // Importando Bibliotecas
 import {Link } from "react-router-dom";
-import { Container, Row, Col } from "react-bootstrap";
+import { Container, Row } from "react-bootstrap";
 import Button from 'react-bootstrap/Button';
 import { Link as Scroll, Element } from 'react-scroll';
 
